8a5ab98e0299b2669eb2c82e4a739a8a739745ad
Kévin <kev.aubree@gmail.com>
2025-02-25 02:06:18 +0100

    feat: delete folder dist

--- dist/types/stock.types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StockAlertSeverity = exports.StockAlertType = void 0;
-const client_1 = require("@prisma/client");
-Object.defineProperty(exports, "StockAlertType", { enumerable: true, get: function () { return client_1.StockAlertType; } });
-Object.defineProperty(exports, "StockAlertSeverity", { enumerable: true, get: function () { return client_1.StockAlertSeverity; } });
